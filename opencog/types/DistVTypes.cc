1e877b1a1a14f6b9c3fae40aedf02d2cf91da6af
Roman Treutlein <roman@singularitynet.io>
2019-08-16 18:18:00 +0200

    Added Types

---
+++ opencog/types/DistVTypes.cc
@@ -0,0 +1,14 @@
+/**
+ * DistVTypes.cc
+ *
+ * Atom Types used for Distributional Values.
+ *
+ * Copyright (c) 2019 Roman Treutlein <roman@singularitynet.io>
+ */
+
+#include "opencog/types/atom_types.definitions"
+
+#define INHERITANCE_FILE "opencog/types/atom_types.inheritance"
+#define INITNAME distv_types_init
+
+#include <opencog/atoms/atom_types/atom_types.cc>
